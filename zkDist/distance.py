272f4edb5bbe103b24b8702096954accffea50b2
Bilal-01 <mbilalaziz.01@gmail.com>
2023-11-19 17:20:17 +0500

    zero knowledge distance completed

--- zkDist/distance.py
+++ zkDist/distance.py
@@ -2,32 +2,43 @@ import subprocess
 import json
 import pandas as pd
 
-
-df = pd.read_csv('train.csv', header=None)
-
-flattened_data = list(map(str, df.values.ravel()))
-
-with open('input.json', 'w') as f:
-    json.dump(flattened_data, f)
-
-def getArgsFromJson(datapoint):
+def getArgsFromJson():
     with open('input.json', 'r') as f:
         data = json.load(f)
 
-    data.extend(map(str, datapoint))
-
     data = [int(x) for x in data]
     arguments = list(map(str, data))
+    arguments.pop()
     return arguments
 
+def minMaxNormalizationAndInteger(df):
+    for column in df.columns:
+        if df[column].dtype == 'float64':
 
-def zkDistance(datapoint):
-    arguments = getArgsFromJson(datapoint)
-    rows = df.shape[0]
-    cols = df.shape[1]
+            min_val = df[column].min()
+            max_val = df[column].max()
+            df[column] = (df[column] - min_val) / (max_val - min_val)
+            df[column] = df[column].round(8)
+            df[column] = (df[column] * 10**8).astype(int)
 
-    print(rows)
-    print(cols)
+    return df
+
+def zkDistance(datapoint, df):
+    
+    datapoint.append(-1)
+    df.loc[len(df)] = datapoint   
+ 
+    normd_df = minMaxNormalizationAndInteger(df)
+
+    flattened_data = list(map(str, normd_df.values.ravel()))
+    with open('input.json', 'w') as f:
+        json.dump(flattened_data, f)
+
+    arguments = getArgsFromJson()
+
+    rows = normd_df.shape[0] - 1
+    cols = normd_df.shape[1]
+
 
     with open('size.zok', 'w') as f:
         f.write('const u32 rows = {};\n'.format(rows))
@@ -44,7 +55,7 @@ def zkDistance(datapoint):
     
     print(proof)
 
+df = pd.read_csv('train.csv', header=None)
 
 
-
-zkDistance([6, 3])
+zkDistance([6, 3], df)
