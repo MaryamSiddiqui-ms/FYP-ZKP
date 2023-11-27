c7204cced72af5e2bd5f6f7dfa267c6c767f6d9a
Bilal-01 <mbilalaziz.01@gmail.com>
2023-11-27 19:53:29 +0500

    proof composition for zkDist

--- ProofComposition/proof_composition.py
+++ ProofComposition/proof_composition.py
@@ -4,8 +4,8 @@ import subprocess
 def load_composite_proof(paths):
 
     size = len(paths)
-    with open('size.zok', 'w') as f:
-        f.write('const u32 size = {};\n'.format(size))
+    # with open('size.zok', 'w') as f:
+    #     f.write('const u32 size = {};\n'.format(size))
 
     combined_proofs = []
     combined_vk = []
@@ -16,7 +16,7 @@ def load_composite_proof(paths):
         with open(proof_path, 'r') as file:
             proof_data = json.load(file)
         
-        proof_keys = {key: proof_data[key] for key in ('inputs', 'proof')}
+        proof_keys = {key: proof_data[key] for key in ('proof', 'inputs')}
         input_length = len(proof_keys['inputs'])
         print(input_length)
         combined_proofs.append(proof_keys)
@@ -27,24 +27,25 @@ def load_composite_proof(paths):
         with open(verification, 'r') as file:
             verification_data = json.load(file)
         
+        print(verification_data)
         verification_keys = {key: verification_data[key] for key in verification_data.keys() - {'scheme', 'curve'}}
         combined_vk.append(verification_keys)
 
     combined_data = [combined_proofs, combined_vk]
 
-    # for path in paths:
-    #     proof_path = path + '/proof.json'
-    #     verification_path = path + '/verification.key'
+    # # for path in paths:
+    # #     proof_path = path + '/proof.json'
+    # #     verification_path = path + '/verification.key'
     
-    #     with open(proof_path, 'r') as file:
-    #         proof_data = json.load(file)
+    # #     with open(proof_path, 'r') as file:
+    # #         proof_data = json.load(file)
 
-    #     with open(verification_path, 'r') as file:
-    #         verification_data = json.load(file)
+    # #     with open(verification_path, 'r') as file:
+    # #         verification_data = json.load(file)
 
-    #     proof_keys = {key: proof_data[key] for key in ('inputs', 'proof')}
-    #     verification_keys = {key: verification_data[key] for key in verification_data.keys() - {'scheme', 'curve'}}
-    #     combined_data = [proof_keys, verification_keys]
+    # #     proof_keys = {key: proof_data[key] for key in ('inputs', 'proof')}
+    # #     verification_keys = {key: verification_data[key] for key in verification_data.keys() - {'scheme', 'curve'}}
+    # #     combined_data = [proof_keys, verification_keys]
 
     with open('gm17.json', 'w') as file:
         json.dump(combined_data, file)
@@ -55,5 +56,5 @@ def run_proof(paths):
     load_composite_proof(paths)
 
 if __name__ == "__main__":
-    paths = ['../zkDist', '../zkSort', '../zkMaxLabel']
+    paths = ['../zkDist']
     run_proof(paths)
