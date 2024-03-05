de86a0a77cda481b0cde6d36d499a052379b8d9e
Bilal-01 <mbilalaziz.01@gmail.com>
2024-03-05 16:34:01 +0500

    conversion from float to int_str

--- models/CNN/cnn_main.py
+++ models/CNN/cnn_main.py
@@ -6,9 +6,16 @@ def getModelParams(file_path):
         
     return weights_dict
 
+def convert_to_str(arr):
+    str_arr = np.vectorize(lambda x: str(int(x * math.pow(10, 8))))(arr)
+    return str_arr
+
 def generateProofCnn(input_image):
     params_file = 'weights_4.json'
     weights_dict = getModelParams(params_file)
+    
+    weights_dict = convert_to_str(weights_dict)
+    input_image = convert_to_str(input_image)
     
     model = CNN(weights_dict, input_image)
     
